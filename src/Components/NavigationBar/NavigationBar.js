cad3f9b7b818eee6bbca6b55fbbeba01fbfddcc1
Oğuz Uğurdoğan <ogi.ugurdogan@hotmail.com>
2022-08-09 22:25:07 +0300

    Home navigation update
     - Added navigation feature to BulutMd text

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,15 +1,21 @@
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import "./NavigationBar.css";
 
 // Her sayfada bulunan giris yapilmadan once giris veya uye olma tuslarinin oldugu
 // giris yapildiktan sonra hesap ve cikis yap gibi tuslarinin oldugu component
 
 const NavigationBar = (props) => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="navigation-bar vertical-center flex-container">
       <div className="inner-container flex-container">
         <div className="title-container">
-          <span className="title">BulutMD</span>
+          <span className="title" onClick={()=>{
+            navigate("/");
+          }}>BulutMD</span>
         </div>
         <div className="log-container horizontal-center flex-container">
           <Button className="login-button" variant="text">
